c64d74ac372d21d0f0501e60d5487df84e4690c3
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-01-13 09:47:18 -0800

    add a dotnet-nuget3 utility script

--- scripts/compile/compile-stage.ps1
+++ scripts/compile/compile-stage.ps1
@@ -94,6 +94,9 @@ _cmd "$RepoRoot\scripts\crossgen\crossgen_roslyn.cmd ""$OutputDir"""
 # Copy dnx into stage OutputDir
 cp -rec "$DnxRoot\" "$OutputDir\bin\dnx\"
 
+# Copy in the dotnet-nuget3 script
+cp "$RepoRoot\scripts\dotnet-nuget3.cmd" "$OutputDir\bin\dotnet-nuget3.cmd"
+
 # Copy in the dotnet-dnx script
 cp "$RepoRoot\scripts\dotnet-dnx.cmd" "$OutputDir\bin\dotnet-dnx.cmd"
 
--- scripts/compile/compile-stage.sh
+++ scripts/compile/compile-stage.sh
@@ -109,6 +109,10 @@ chmod -R a+r $OUTPUT_DIR
 # Copy DNX in to OUTPUT_DIR
 cp -R $DNX_ROOT $OUTPUT_DIR/bin/dnx
 
+# Copy and CHMOD the dotnet-nuget3 script
+cp $REPOROOT/scripts/dotnet-nuget3.sh $OUTPUT_DIR/bin/dotnet-nuget3
+chmod a+x $OUTPUT_DIR/bin/dotnet-nuget3
+
 # Copy and CHMOD the dotnet-dnx script
 cp $REPOROOT/scripts/dotnet-dnx.sh $OUTPUT_DIR/bin/dotnet-dnx
 chmod a+x $OUTPUT_DIR/bin/dotnet-dnx
